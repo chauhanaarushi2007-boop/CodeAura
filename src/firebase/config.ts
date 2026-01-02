cb4b4a96e8cdfae4f64d3cef7b5ee17bda0a28df
Aarushi chauhan <chauhan.aarushi2007@gmail.com>
2026-01-02 15:52:08 +0000

    Try fixing this error: `Runtime FirebaseError: Firebase: Error (auth/inv

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,4 +1,11 @@
 // This file is intentionally left blank. It will be populated
 // with your Firebase project configuration by the backend.
 // You can also manually add your Firebase config here.
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  apiKey: "AIzaSyXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  authDomain: "your-project-id.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:1234567890abcdef"
+};
